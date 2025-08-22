fc27658fbabf226cb368e667fc2a1ef0c7f98077
Shubham-1417 <e1486333@u.nus.edu>
2025-08-22 12:08:00 +0800

    Enable Text UI Testing

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,21 +1,37 @@
 @ECHO OFF
+chcp 65001 > nul
 
-REM create bin directory if it doesn't exist
-if not exist ..\bin mkdir ..\bin
+REM Set script directory
+SET SCRIPT_DIR=%~dp0
 
-REM delete output from previous run
-if exist ACTUAL.TXT del ACTUAL.TXT
+REM Set project paths
+SET BIN_DIR=%SCRIPT_DIR%..\bin
+SET SRC_DIR=%SCRIPT_DIR%..\src\main\java
 
-REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
-IF ERRORLEVEL 1 (
+REM Create bin directory if it doesn't exist
+if not exist "%BIN_DIR%" mkdir "%BIN_DIR%"
+
+REM Delete previous ACTUAL.TXT if it exists
+if exist "%SCRIPT_DIR%ACTUAL.TXT" del "%SCRIPT_DIR%ACTUAL.TXT"
+
+echo Compiling...
+javac -Xlint:none -d "%BIN_DIR%" "%SRC_DIR%\V.java" "%SRC_DIR%\Task.java" "%SRC_DIR%\Todo.java" "%SRC_DIR%\Deadline.java" "%SRC_DIR%\Event.java"
+if errorlevel 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
-REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+echo Running tests...
+java -classpath "%BIN_DIR%" V < "%SCRIPT_DIR%input.txt" > "%SCRIPT_DIR%ACTUAL.TXT" 2> "%SCRIPT_DIR%error.log"
+echo Test run finished.
 
-REM compare the output to the expected output
-FC ACTUAL.TXT EXPECTED.TXT
+echo Comparing with expected output...
+FC /N "%SCRIPT_DIR%ACTUAL.TXT" "%SCRIPT_DIR%EXPECTED.TXT"
+
+if errorlevel 1 (
+    echo ********** TEST FAILED **********
+    exit /b 1
+) else (
+    echo ********** TEST PASSED **********
+    exit /b 0
+)
